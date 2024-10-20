8945c8210ac1971b0b6338a03a9bc25e09e22b53
ortegadaniel588 <127459678+ortegadaniel588@users.noreply.github.com>
2024-10-20 11:51:58 -0500

    Correccion

--- WebAppConsultorioJuridico/Data/SeguimientoDat.cs
+++ WebAppConsultorioJuridico/Data/SeguimientoDat.cs
@@ -1,5 +1,8 @@
-﻿using System;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 
@@ -23,7 +26,7 @@ namespace Data
             return objData;
         }
         //Metodo para guardar un nuevo Seguimiento
-        public bool saveSeguimiento(string _codigo, string _fecha, string _accionrealizada, string _relevancia, string _evidencia, string _comentario, string _estado)
+        public bool saveSeguimiento(int _caso_id, string _fecha_actualizacion, string _proceso, string _descripcion, string _estado)
         {
             bool executed = false;
             int row;
@@ -32,15 +35,12 @@ namespace Data
             objSelectCmd.Connection = objPer.openConnection();
             objSelectCmd.CommandText = "spInsertSeguimiento";
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            
-            objSelectCmd.Parameters.Add("p_codigo", MySqlDbType.VarString).Value = _codigo;
-            objSelectCmd.Parameters.Add("p_fecha", MySqlDbType.VarString).Value = _fecha;
-            objSelectCmd.Parameters.Add("p_accionrealizada", MySqlDbType.VarString).Value = _accionrealizada;
-            objSelectCmd.Parameters.Add("p_razon", MySqlDbType.VarString).Value = _razon;
-            objSelectCmd.Parameters.Add("p_relevancia", MySqlDbType.VarString).Value = _relevancia;
-            objSelectCmd.Parameters.Add("p_evidencia", MySqlDbType.VarString).Value = _evidencia;
-            objSelectCmd.Parameters.Add("p_comentario", MySqlDbType.VarString).Value = _comentario;
-            objSelectCmd.Parameters.Add("p_estado", MySqlDbType.VarString).Value = _estado; 
+
+            objSelectCmd.Parameters.Add("p_caso_id", MySqlDbType.Int32).Value = _caso_id;
+            objSelectCmd.Parameters.Add("p_fecha_actualizacion", MySqlDbType.DateTime).Value = _fecha_actualizacion;
+            objSelectCmd.Parameters.Add("p_proceso", MySqlDbType.VarString).Value = _proceso;
+            objSelectCmd.Parameters.Add("p_descripcion", MySqlDbType.VarString).Value = _descripcion;
+            objSelectCmd.Parameters.Add("p_estado", MySqlDbType.VarString).Value = _estado;
 
             try
             {
@@ -60,7 +60,7 @@ namespace Data
         }
 
         //Metodo para actulizar un Seguimiento
-        public bool updateSeguimiento(int _caso_id, string _codigo, string _fecha, string _accionrealizada, string _relevancia, string _evidencia, string _comentario, string _estado)
+        public bool updateSeguimiento(int _id, int _caso_id, string _fecha_actualizacion, string _proceso, string _descripcion, string _estado)
         {
             bool executed = false;
             int row;
@@ -70,14 +70,13 @@ namespace Data
             objSelectCmd.CommandText = "spUpdateSeguimiento";
             objSelectCmd.CommandType = CommandType.StoredProcedure;
 
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
             objSelectCmd.Parameters.Add("p_caso_id", MySqlDbType.Int32).Value = _caso_id;
-            objSelectCmd.Parameters.Add("p_fecha", MySqlDbType.VarString).Value = _fecha;
-            objSelectCmd.Parameters.Add("p_accionrealizada", MySqlDbType.VarString).Value = _accionrealizada;
-            objSelectCmd.Parameters.Add("p_razon", MySqlDbType.VarString).Value = _razon;
-            objSelectCmd.Parameters.Add("p_relevancia", MySqlDbType.VarString).Value = _relevancia;
-            objSelectCmd.Parameters.Add("p_evidencia", MySqlDbType.VarString).Value = _evidencia;
-            objSelectCmd.Parameters.Add("p_comentario", MySqlDbType.VarString).Value = _comentario;
+            objSelectCmd.Parameters.Add("p_fecha_actualizacion", MySqlDbType.DateTime).Value = _fecha_actualizacion;
+            objSelectCmd.Parameters.Add("p_proceso", MySqlDbType.VarString).Value = _proceso;
+            objSelectCmd.Parameters.Add("p_descripcion", MySqlDbType.VarString).Value = _descripcion;
             objSelectCmd.Parameters.Add("p_estado", MySqlDbType.VarString).Value = _estado;
+
 
             try
             {
@@ -105,7 +104,7 @@ namespace Data
             objSelectCmd.Connection = objPer.openConnection();
             objSelectCmd.CommandText = "spDeleteSeguimiento"; //nombre del procedimiento almacenado
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objSelectCmd.Parameters.Add("p_caso_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _id;
             try
             {
                 row = objSelectCmd.ExecuteNonQuery();

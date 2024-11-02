7eaf83b9ed70a179609e59edd90888da6413c877
LuisgMarin <marnluis189@gmail.com>
2024-11-02 03:20:14 -0500

    feat: create web form Permiso

---
+++ WebAppConsultorioJuridico/Presentation/WFPermiso.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Main.Master" AutoEventWireup="true" CodeBehind="WFPermiso.aspx.cs" Inherits="Presentation.WFPermiso" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:TextBox ID="TBId" runat="server" Visible="false"></asp:TextBox>
+    <%--Nombre--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese el Nombre"></asp:Label>
+    <asp:TextBox ID="TBNombre" runat="server"></asp:TextBox>
+    <br />
+    <%--Descripción--%>
+    <asp:Label ID="Label2" runat="server" Text="Ingrese la Descripción"></asp:Label>
+    <asp:TextBox ID="TBDescripcion" runat="server"></asp:TextBox>
+    <br />
+    <%--Botones Guardar y Actualizar--%>
+    <div>
+        <asp:Button ID="BtnSave" runat="server" Text="Guardar" OnClick="BtnSave_Click" />
+        <asp:Button ID="BtnUpdate" runat="server" Text="Actualizar" OnClick="BtnUpdate_Click" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    <%--Lista de Permisos--%>
+    <div>
+        <asp:GridView ID="GVPermisos" runat="server" AutoGenerateColumns="False" OnRowCommand="GVPermisos_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="idpermiso" HeaderText="ID" />
+                <asp:BoundField DataField="nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="descripcion" HeaderText="Descripción" />
+                <asp:ButtonField ButtonType="Button" CommandName="Edit" Text="Editar" />
+                <asp:ButtonField ButtonType="Button" CommandName="Delete" Text="Eliminar" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ WebAppConsultorioJuridico/Presentation/WFPermiso.aspx.cs
@@ -0,0 +1,99 @@
+﻿using Logic;
+using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class WFPermiso : System.Web.UI.Page
+    {
+        PermisoLog objPermiso = new PermisoLog();
+
+        private int _idPermiso;
+        private string _nombre, _descripcion;
+        private bool executed = false;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowPermisos();
+            }
+        }
+
+        private void ShowPermisos()
+        {
+            DataSet ds = objPermiso.ShowPermisos();
+            GVPermisos.DataSource = ds;
+            GVPermisos.DataBind();
+        }
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _nombre = TBNombre.Text;
+            _descripcion = TBDescripcion.Text;
+
+            executed = objPermiso.SavePermiso(_nombre, _descripcion);
+
+            if (executed)
+            {
+                LblMsg.Text = "El permiso se guardó exitosamente!";
+                ShowPermisos();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar";
+            }
+        }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            _idPermiso = Convert.ToInt32(TBId.Text);
+            _nombre = TBNombre.Text;
+            _descripcion = TBDescripcion.Text;
+
+            executed = objPermiso.UpdatePermiso(_idPermiso, _nombre, _descripcion);
+
+            if (executed)
+            {
+                LblMsg.Text = "El permiso se actualizó exitosamente!";
+                ShowPermisos();
+            }
+            else
+            {
+                LblMsg.Text = "Error al actualizar";
+            }
+        }
+
+        protected void GVPermisos_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Edit")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = GVPermisos.Rows[index];
+                TBId.Text = row.Cells[0].Text;
+                TBNombre.Text = row.Cells[1].Text;
+                TBDescripcion.Text = row.Cells[2].Text;
+            }
+            else if (e.CommandName == "Delete")
+            {
+                int index = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = GVPermisos.Rows[index];
+                int idPermiso = Convert.ToInt32(row.Cells[0].Text);
+
+                executed = objPermiso.DeletePermiso(idPermiso);
+
+                if (executed)
+                {
+                    LblMsg.Text = "El permiso se eliminó exitosamente!";
+                    ShowPermisos();
+                }
+                else
+                {
+                    LblMsg.Text = "Error al eliminar";
+                }
+            }
+        }
+    }
+}

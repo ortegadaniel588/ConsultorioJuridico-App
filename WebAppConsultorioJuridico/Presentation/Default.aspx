ec83bab73cf61b212a2411c9060b1dbb49d30d01
ortegadaniel588 <ortegadaniel588@gmail.com>
2024-11-15 23:30:41 -0500

    Se crearon Default y Index

---
+++ WebAppConsultorioJuridico/Presentation/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Presentation.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <!-- Imagen de cargando... -->
+        <div id="loadingGif" style="display: none;">
+            <img src="resources/images/loading-7528_128.gif" alt="Cargando..." />
+        </div>
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="">Iniciar Sesion</asp:Label><br />
+            <asp:Label ID="Label1" runat="server" Text="">Correo</asp:Label>
+            <asp:TextBox ID="TBUsuario" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label3" runat="server" Text="">Contraseña</asp:Label>
+            <asp:TextBox ID="TBContrasena" runat="server" TextMode="Password"></asp:TextBox><br />
+            <asp:Button ID="BtnIniciar" runat="server" Text="Iniciar" OnClick="BtnIniciar_Click" OnClientClick="showLoading();" /><br />
+            <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+    <%--Script para mostrar y ocultar la animacion de cargando...--%>
+    <script type="text/javascript">
+        function showLoading() {
+            document.getElementById("loadingGif").style.display = "block";
+        }
+
+        function hideLoading() {
+            document.getElementById("loadingGif").style.display = "none";
+        }
+    </script>
+</body>
+</html>
---
+++ WebAppConsultorioJuridico/Presentation/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using Logic;
+using Model;
+using SimpleCrypto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        UsuariosLog objUserLog = new UsuariosLog();
+        User objUser = new User();
+
+        private string _usuario;
+        private string _contrasena;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            // Muestra la imagen de cargando antes de procesar la encriptación
+            ScriptManager.RegisterStartupScript(this, GetType(), "showLoading", "showLoading();", true);
+
+            ICryptoService cryptoService = new PBKDF2();
+            _usuario = TBUsuario.Text;
+            _contrasena = TBContrasena.Text;
+
+            objUser = objUserLog.showUsersMail(_usuario);// Busca el correo del usuario
+
+            if (objUser != null)
+            {
+                if (objUser.State == "Activo")// Verifica si el usuario está activo
+                {
+                    // Almacena objUser en la sesión
+                    Session["User"] = objUser;
+
+                    string passEncryp = cryptoService.Compute(_contrasena, objUser.Salt);
+                    if (cryptoService.Compare(objUser.Contrasena, passEncryp))
+                    {
+                        FormsAuthentication.RedirectFromLoginPage("WFInicio.aspx", true);
+                        TBUsuario.Text = "";
+                        TBContrasena.Text = "";
+                    }
+                    else
+                    {
+                        LblMsg.Text = "Correo o Contraseña Incorrectos!";
+                    }
+                }
+                else
+                {
+                    LblMsg.Text = "El usuario no está activo. Contacte al administrador.";
+                }
+
+            }
+            else
+            {
+                LblMsg.Text = "Correo o Contraseña Incorrectos!";
+            }
+            // Oculta la imagen de cargando al terminar el procesamiento
+            ScriptManager.RegisterStartupScript(this, GetType(), "hideLoading", "hideLoading();", true);
+        }
+    }
+}

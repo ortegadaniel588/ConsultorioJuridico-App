de49a6717d73f0331815c0d0799fdb9477bc88da
LuisgMarin <marnluis189@gmail.com>
2024-11-16 01:26:24 -0500

    fix PersonaLog metodos data

--- WebAppConsultorioJuridico/Logic/PersonasLog.cs
+++ WebAppConsultorioJuridico/Logic/PersonasLog.cs
@@ -8,24 +8,24 @@ namespace Logic
     {
         PersonaDat objPersona = new PersonaDat();
 
-        public DataSet ShowPersonasDDL()
+        public DataSet showPersonasDDL()
         {
             return objPersona.showPersonasDDL();
         }
 
-        public DataSet ShowPersonas()
+        public DataSet showPersonas()
         {
-            return objPersona.ShowPersonas();
+            return objPersona.showPersonas();
         }
 
-        public bool SavePersona(string nombres, string apellidos, string tipodocumento, string documento, string genero, string estadocivil, string lugarNacimiento, DateTime fechaNacimiento, string telefono, string celular, string correo, string direccion, string estado, string ocupacion, string nivelEducacion)
+        public bool savePersona(string nombres, string apellidos, string tipodocumento, string documento, string genero, string estadocivil, string lugarNacimiento, DateTime fechaNacimiento, string telefono, string telefono2, string correo, string direccion, string estrato, string ocupacion, string nivelEducacion)
         {
-            return objPersona.SavePersona(nombres, apellidos, tipodocumento, documento, genero, estadocivil, lugarNacimiento, fechaNacimiento, telefono, celular, correo, direccion, estado, ocupacion, nivelEducacion);
+            return objPersona.savePersona(nombres, apellidos, tipodocumento, documento, genero, estadocivil, lugarNacimiento, fechaNacimiento, telefono, telefono2, correo, direccion, estrato, ocupacion, nivelEducacion);
         }
 
-        public bool UpdatePersona(int idpersona, string nombres, string apellidos, string tipodocumento, string documento, string genero, string estadocivil, string lugarNacimiento, DateTime fechaNacimiento, string telefono, string celular, string correo, string direccion, string estado, string ocupacion, string nivelEducacion)
+        public bool updatePersona(int idpersona, string nombres, string apellidos, string tipodocumento, string documento, string genero, string estadocivil, string lugarNacimiento, DateTime fechaNacimiento, string telefono, string telefono2, string correo, string direccion, string estrato, string ocupacion, string nivelEducacion)
         {
-            return objPersona.UpdatePersona(idpersona, nombres, apellidos, tipodocumento, documento, genero, estadocivil, lugarNacimiento, fechaNacimiento, telefono, celular, correo, direccion, estado, ocupacion, nivelEducacion);
+            return objPersona.updatePersona(idpersona, nombres, apellidos, tipodocumento, documento, genero, estadocivil, lugarNacimiento, fechaNacimiento, telefono, telefono2, correo, direccion, estrato, ocupacion, nivelEducacion);
         }
 
         public bool DeletePersona(int idpersona)

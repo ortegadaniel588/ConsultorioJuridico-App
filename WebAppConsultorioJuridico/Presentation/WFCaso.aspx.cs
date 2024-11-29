3be078aede8ed8ce763ad3a838b9644837b39c90
ortegadaniel588 <ortegadaniel588@gmail.com>
2024-11-29 11:47:03 -0500

    Se creo método validación
    No se invoca el método

--- WebAppConsultorioJuridico/Presentation/WFCaso.aspx.cs
+++ WebAppConsultorioJuridico/Presentation/WFCaso.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using Logic;
+using Model;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -30,10 +31,18 @@ namespace Presentation
         private string _complejidad;
         private int _empleado;
         private bool execute = false;
+
+        public bool _showEditButton { get; set; } = false;
+        public bool _showDeleteButton { get; set; } = false;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!Page.IsPostBack)
             {
+                /*BtnSave.Visible = false;
+                BtnUpdate.Visible = false;
+                FrmCaso.Visible = false;
+                PanelAdmin.Visible = false;*/
                 //showCaso();
                 showEmpresaDDL();
                 showEstadoDDL();
@@ -41,6 +50,7 @@ namespace Presentation
                 showEmpleadoDDL();
 
             }
+            //validatePermissionRol();
         }
         /*Se elimino este mtodo para añadir el datatable JavaScrit*/
         /*private void showCaso()
@@ -50,7 +60,7 @@ namespace Presentation
             GVEmpresa.DataSource = objData;
             GVEmpresa.DataBind();
         }*/
-        
+
         [WebMethod]
         public static object ListCasos()
         {
@@ -99,6 +109,141 @@ namespace Presentation
 
             // Invocar al método para eliminar el producto y devolver el resultado
             return objCas.deleteCaso(id);
+        }
+
+        private void validatePermissionRol()
+        {
+            // Se Obtiene el usuario actual desde la sesión
+            var objUser = (User)Session["User"];
+
+            // Variable para acceder a la MasterPage y modificar la visibilidad de los enlaces.
+            var masterPage = (Main)Master;
+
+            if (objUser == null)
+            {
+                // Redirige a la página de inicio de sesión si el usuario no está autenticado
+                Response.Redirect("WFDefault.aspx");
+                return;
+            }
+            // Obtener el rol del usuario
+            var userRole = objUser.Rol.Nombre;
+
+            if (userRole == "Administrador")
+            {
+                //LblMsg.Text = "Bienvenido, Administrador!";
+
+                foreach (var permiso in objUser.Permisos)
+                {
+                    switch (permiso.Nombre)
+                    {
+                        case "CREAR":
+                            FrmCaso.Visible = true;// Se pone visible el formulario
+                            BtnSave.Visible = true;// Se pone visible el boton guardar
+                            break;
+                        case "ACTUALIZAR":
+                            FrmCaso.Visible = true;
+                            BtnUpdate.Visible = true;// Se pone visible el boton actualizar
+                            PanelAdmin.Visible = true;// Se pone visible el panel
+                            _showEditButton = true;// Se pone visible el boton editar dentro de la datatable
+                            break;
+                        case "MOSTRAR":
+                            //LblMsg.Text += " Tienes permiso de Mostrar!";
+                            PanelAdmin.Visible = true;
+                            break;
+                        case "ELIMINAR":
+                            //LblMsg.Text += " Tienes permiso de Eliminar!";
+                            PanelAdmin.Visible = true;
+                            _showDeleteButton = true;// Se pone visible el boton eliminar dentro de la datatable
+                            break;
+                        default:
+                            // Si el permiso no coincide con ninguno de los casos anteriores
+                            LblMsj.Text += $" Permiso desconocido: {permiso.Nombre}";
+                            break;
+                    }
+                }
+            }
+            else if (userRole == "Gerente")
+            {
+                //LblMsg.Text = "Bienvenido, Gerente!";
+
+                masterPage.linkUser.Visible = false;// Se oculta el enlace de Usuario
+                //masterPage.linkPermission.Visible = false; // Se oculta el enlace Permiso 
+                //masterPage.linkPermissionRol.Visible = false;// Se oculta el enlace de Permiso Rol
+
+                foreach (var permiso in objUser.Permisos)
+                {
+                    switch (permiso.Nombre)
+                    {
+                        case "CREAR":
+                            FrmCaso.Visible = true;
+                            BtnSave.Visible = true;
+                            PanelAdmin.Visible = true;
+                            break;
+                        case "ACTUALIZAR":
+                            FrmCaso.Visible = true;
+                            BtnUpdate.Visible = true;
+                            PanelAdmin.Visible = true;
+                            _showEditButton = true;
+                            break;
+                        case "MOSTRAR":
+                            //LblMsg.Text += " Tienes permiso de Mostrar!";
+                            PanelAdmin.Visible = true;
+                            break;
+                        case "ELIMINAR":
+                            //LblMsg.Text += " Tienes permiso de Eliminar!";
+                            PanelAdmin.Visible = true;
+                            _showDeleteButton = true;
+                            break;
+                        default:
+                            // Si el permiso no coincide con ninguno de los casos anteriores
+                            LblMsj.Text += $" Permiso desconocido: {permiso.Nombre}";
+                            break;
+                    }
+                }
+
+            }
+            else if (userRole == "Secretaria")
+            {
+                //LblMsg.Text = "Bienvenido, Secretaria!";
+                masterPage.linkUser.Visible = false;
+                //masterPage.linkPermission.Visible = false;
+                //masterPage.linkPermissionRol.Visible = false;
+
+                foreach (var permiso in objUser.Permisos)
+                {
+                    switch (permiso.Nombre)
+                    {
+                        case "CREAR":
+                            FrmCaso.Visible = true;
+                            BtnSave.Visible = true;
+                            PanelAdmin.Visible = true;
+                            break;
+                        case "ACTUALIZAR":
+                            FrmCaso.Visible = true;
+                            BtnUpdate.Visible = true;
+                            PanelAdmin.Visible = true;
+                            _showEditButton = true;
+                            break;
+                        case "MOSTRAR":
+                            PanelAdmin.Visible = true;
+                            break;
+                        case "ELIMINAR":
+                            PanelAdmin.Visible = true;
+                            _showDeleteButton = true;
+                            break;
+                        default:
+                            // Si el permiso no coincide con ninguno de los casos anteriores
+                            LblMsj.Text += $" Permiso desconocido: {permiso.Nombre}";
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                // Si el rol no es reconocido, se deniega el acceso
+                LblMsj.Text = "Rol no reconocido. No tienes permisos suficientes para acceder a esta página.";
+                Response.Redirect("WFInicio.aspx");
+            }
         }
 
         private void showEmpresaDDL()

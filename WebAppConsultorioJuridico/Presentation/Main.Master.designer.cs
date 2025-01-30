17397fb66dbed32d93cb11c5a5321bd2b3f3759d
ortegadaniel588 <ortegadaniel588@gmail.com>
2025-01-30 00:40:15 -0500

    s

--- WebAppConsultorioJuridico/Presentation/Main.Master.designer.cs
+++ WebAppConsultorioJuridico/Presentation/Main.Master.designer.cs
@@ -24,15 +24,6 @@ namespace Presentation
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Control linkEmpleado.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlAnchor linkEmpleado;
-
-        /// <summary>
         /// Control linkEmpresa2.
         /// </summary>
         /// <remarks>
